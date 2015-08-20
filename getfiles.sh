405e85ffe4d98cb61e215c90ca6baf088cc807dc
Boris Joffe <boris@joff3.com>
2015-08-19 22:19:03 -0700

    update getfiles for X config files

--- getfiles.sh
+++ getfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # temporarily removed bashrc - restore from backup later
-cp ~/.{bashrc,screenrc,vimrc,zshrc,tmux.conf} .
+cp ~/.{bashrc,screenrc,vimrc,zshrc,tmux.conf,xbindkeysrc,Xresources,xinitrc} .
 
 #cp -R ~/.vim/ .
