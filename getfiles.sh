e93e3b2eadd49cc6a8577fd072afab78c8ac417d
Boris Joffe <boris@joff3.com>
2015-08-21 00:58:55 -0700

    update getfiles to pull in openbox config file

--- getfiles.sh
+++ getfiles.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+SCRIPT_DIR=`pwd`
 
 # temporarily removed bashrc - restore from backup later
 cp ~/.{bashrc,screenrc,vimrc,zshrc,tmux.conf,xbindkeysrc,Xresources,xinitrc} .
+cd $HOME
+cp --parents .config/openbox/rc.xml $SCRIPT_DIR
 
 #cp -R ~/.vim/ .
