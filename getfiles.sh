943108a54006eec30d626d08742a6b82ff90de4b
Boris Joffe <boris@joff3.com>
2015-09-22 09:59:25 -0700

    copy fish config automatically

--- getfiles.sh
+++ getfiles.sh
@@ -5,5 +5,6 @@ SCRIPT_DIR=`pwd`
 cp ~/.{bashrc,screenrc,vimrc,zshrc,tmux*.conf,xbindkeysrc,Xresources,xinitrc} .
 cd $HOME
 cp --parents .config/openbox/rc.xml $SCRIPT_DIR
+cp --parents .config/fish/{functions/,}*.fish $SCRIPT_DIR
 
 #cp -R ~/.vim/ .
