ea5d6efe2806006c79cf80d488a06634c5eb7cb7
Boris Joffe <boris@joff3.com>
2015-10-01 11:17:47 -0700

    add html5 tidy config file and env var

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,6 @@
 setenv SSH_ENV $HOME/.ssh/environment
 set -gx PATH $PATH /data/anaconda3/bin /data/android/android-sdk-linux/tools/ /data/android/android-sdk-linux/platform-tools/
 setenv ANDROID_HOME /data/android/sdk
+setenv HTML_TIDY "$HOME/.tidyrc"
 
 ssh_agent_start
--- getfiles.sh
+++ getfiles.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR=`pwd`
 
 # temporarily removed bashrc - restore from backup later
-cp ~/.{aliases,shellrc,bashrc,screenrc,vimrc,zshrc,tmux*.conf,xbindkeysrc,Xresources*,xinitrc} .
+cp ~/.{aliases,shellrc,bashrc,screenrc,vimrc,zshrc,tmux*.conf,xbindkeysrc,Xresources*,xinitrc,tidyrc} .
 cd $HOME
 cp --parents .config/openbox/rc.xml $SCRIPT_DIR
 cp --parents .config/fish/{functions/,}*.fish $SCRIPT_DIR
