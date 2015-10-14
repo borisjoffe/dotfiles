ce822d0f3e5f6e5ec5126639e77d91b15e715e0c
Boris Joffe <boris@joff3.com>
2015-10-14 13:01:39 -0700

    replace 'set -gx' with 'setenv' to be consistent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 #!/bin/fish
 
 setenv SSH_ENV $HOME/.ssh/environment
-set -gx PATH $PATH /data/anaconda3/bin /data/android/android-sdk-linux/tools/ /data/android/android-sdk-linux/platform-tools/
+setenv PATH $PATH /data/anaconda3/bin /data/android/android-sdk-linux/tools/ /data/android/android-sdk-linux/platform-tools/
 setenv ANDROID_HOME /data/android/sdk
 setenv HTML_TIDY "$HOME/.tidyrc"
 setenv pacman_program pacaur # make pacmatic use pacaur so you can get news and update AUR at the same time
