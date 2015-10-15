4fc8196dd62246982888e827a57e4bed3ef916f1
Boris Joffe <boris@joff3.com>
2015-10-15 13:46:15 -0700

    update fish initialization of PATH and SOLARIZED env vars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 #!/bin/fish
 
+setenv SOLARIZED 1
 setenv SSH_ENV $HOME/.ssh/environment
-setenv PATH $PATH /data/anaconda3/bin /data/android/android-sdk-linux/tools/ /data/android/android-sdk-linux/platform-tools/
+setenv PATH $PATH $HOME/bin /data/anaconda3/bin /data/android/android-sdk-linux/tools/ /data/android/android-sdk-linux/platform-tools/
 setenv ANDROID_HOME /data/android/sdk
 setenv HTML_TIDY "$HOME/.tidyrc"
 setenv pacman_program pacaur # make pacmatic use pacaur so you can get news and update AUR at the same time
