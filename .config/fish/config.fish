d214d1764ad0ec0022a69070cff23af02fef7125
Boris Joffe <boris@joff3.com>
2015-10-29 08:58:31 -0700

    add fix for fish issue #183

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,13 @@ setenv ANDROID_HOME /data/android/sdk
 setenv HTML_TIDY "$HOME/.tidyrc"
 setenv pacman_program pacaur # make pacmatic use pacaur so you can get news and update AUR at the same time
 
+# Set universal vars here to prevent being overwritten
+# due to hostname bug: https://github.com/fish-shell/fish-shell/issues/183
+set -U fish_greeting ""
+
+# Use below to re-import abbreviations:
+# source ~/code/dotfiles/.config/fish/fish-abbr.fish
+
 ssh_agent_start
 
 # OPAM Configuration - doesn't work - TODO: fix
