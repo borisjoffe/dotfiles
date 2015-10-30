28af52f783f8587894b1f76b07b0da424439cd52
Boris Joffe <boris@joff3.com>
2015-10-30 14:36:39 -0700

    fish - add omf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,3 +14,13 @@ ssh_agent_start
 
 # OPAM Configuration - doesn't work - TODO: fix
 source /home/boris/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+
+
+# Path to Oh My Fish install.
+set -gx OMF_PATH /home/boris/.local/share/omf
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG /home/boris/.config/omf
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
