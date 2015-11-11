247237a1a9bcb541c1476b9686d3380dd03604f7
Boris Joffe <boris@joff3.com>
2015-11-11 08:37:46 -0800

    debian-specific aliases

--- getfiles.sh
+++ getfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR=`pwd`
 
-cp -R ~/.{aliases,shellrc,bashrc,screenrc,vimrc,zsh*,tmux*.conf,xbindkeysrc,Xresources*,xinitrc,tidyrc,editorconfig,gitconfig,gitignore.global} .
+cp -R ~/.{*aliases,shellrc,bashrc,screenrc,vimrc,zsh*,tmux*.conf,xbindkeysrc,Xresources*,xinitrc,tidyrc,editorconfig,gitconfig,gitignore.global} .
 cd $HOME
 cp --parents .config/openbox/rc.xml $SCRIPT_DIR
 cp --parents .config/fish/{functions/,}*.fish $SCRIPT_DIR
