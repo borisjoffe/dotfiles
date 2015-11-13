05e4bcdfc39346cdc5e92fa93aef9c4dde801385
Boris Joffe <boris@joff3.com>
2015-11-13 14:53:44 -0800

    speed up getfiles script - async fish / smart copy
    - get fish abbr asyncronously
    - slightly speed up other file copies

--- getfiles.sh
+++ getfiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR=`pwd`
 
-cp -R ~/.{*aliases,shellrc,bashrc,screenrc,vimrc,zsh*,tmux*.conf,xbindkeysrc,Xresources*,xinitrc,tidyrc,editorconfig,gitconfig,gitignore.global} .
+cp -u -R ~/.{*aliases,shellrc,bashrc,screenrc,vimrc,zsh*,tmux*.conf,xbindkeysrc,Xresources*,xinitrc,tidyrc,editorconfig,gitconfig,gitignore.global} .
 cd $HOME
-cp --parents .config/openbox/rc.xml $SCRIPT_DIR
-cp --parents .config/fish/{functions/,}*.fish $SCRIPT_DIR
-fish -c 'abbr -s > .config/fish/fish-abbr.fish'
+cp -u --parents .config/openbox/rc.xml $SCRIPT_DIR
+cp -u --parents .config/fish/{functions/,}*.fish $SCRIPT_DIR
+fish -c 'abbr -s > .config/fish/fish-abbr.fish' &
 
 #cp -R ~/.vim/ .
