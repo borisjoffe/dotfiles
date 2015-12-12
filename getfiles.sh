3963c4f78f3c5a7cebb74e616822aff431a97a2f
Boris Joffe <boris@joff3.com>
2015-12-11 21:06:18 -0800

    getfiles - copy over flake8

--- getfiles.sh
+++ getfiles.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR=`pwd`
 cp -u -R ~/.{*aliases,shellrc,bashrc,screenrc,vimrc,zsh*,tmux*.conf,xbindkeysrc,Xresources*,xinitrc,tidyrc,editorconfig,jshintrc,eslintrc,gitconfig,gitignore.global} .
 cd $HOME
 cp -u --parents .config/openbox/rc.xml $SCRIPT_DIR
+cp -u --parents .config/flake8 $SCRIPT_DIR
 cp -u --parents .config/fish/{functions/,}*.fish $SCRIPT_DIR
 fish -c 'abbr -s > .config/fish/fish-abbr.fish' &
 
