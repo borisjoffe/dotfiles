7c3cb518fa873d3e0ca8c05b1132bb2adf1834ab
Boris Joffe <boris@joff3.com>
2016-01-24 00:22:28 -0800

    fish - LANG env var, uncomment omf stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,19 +8,22 @@ source ~/bin/solarized-switch.fish
 # due to hostname bug: https://github.com/fish-shell/fish-shell/issues/183
 set -U fish_greeting ""
 
+#set -U LANG en-US.UTF-8
+#set -U LC_ALL en-US.UTF-8
+
 # Use below to re-import abbreviations:
 # source ~/code/dotfiles/.config/fish/fish-abbr.fish
 
 ssh_agent_start
 
 # OPAM Configuration - Manually fixed PATH and MANPATH in ~/.opam/opam-init/variables.fish
-source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+#source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
 
 # Path to Oh My Fish install.
 set -gx OMF_PATH $HOME/.local/share/omf
 
 # Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG $HOME/.config/omf
+set -gx OMF_CONFIG $HOME/.config/omf
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
