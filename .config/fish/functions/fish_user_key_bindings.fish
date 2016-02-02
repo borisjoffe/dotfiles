d65628ec00300dbede8de370a50743db869a4f04
Boris Joffe <boris@joff3.com>
2016-02-01 16:16:36 -0800

    fish - !! and !$

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,26 @@
+function bind_bang
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function bind_dollar
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
 function fish_user_key_bindings
 	bind \e\[8~ end-of-line
 	bind \e\[7~ beginning-of-line
+
+	bind ! bind_bang
+	bind '$' bind_dollar
 end
