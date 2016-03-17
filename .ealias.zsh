e8c226c20c594c7accfca4dea6700e733774c6a3
Boris Joffe <boris@joff3.com>
2016-03-17 11:29:38 -0700

    add aliases for sourcing the current file

--- .ealias.zsh
+++ .ealias.zsh
@@ -34,3 +34,5 @@ bindkey -M emacs ' '     expand-ealias
 bindkey -M emacs '^\034'  force-expand-ealias # Ctrl-\ forces expansion (regardless of preceding chars - useful for global aliases)
 bindkey -M emacs '^ '    magic-space     # control-space to bypass completion
 bindkey -M isearch " "   magic-space # normal space during searches
+
+ealias rea="source $0"
