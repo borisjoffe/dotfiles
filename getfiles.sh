74186edc3b8ce079684faa95174f88dcd6af040b
Boris Joffe <boris@joff3.com>
2016-04-24 15:52:37 -0700

    getfiles - update for better readability and cleaner diffs

--- getfiles.sh
+++ getfiles.sh
@@ -1,14 +1,65 @@
-#!/bin/bash
+#!/usr/bin/zsh
+
 SCRIPT_DIR=`pwd`
 
-cp -u -R ~/.{*aliases,shellrc,bashrc,screenrc,vimrc,zsh*,*.zsh,tmux*.conf,xbindkeysrc,Xresources*,xinitrc,tidyrc,editorconfig,jshintrc,eslintrc,gitconfig,gitignore.global,ackrc,npm_lazy.config.js,npmrc,mongorc.js,env,tmuxp,noderc.js,gtkrc-2.0*} .
+set -ueo pipefail
 cd $HOME
-cp -u --parents .config/openbox/rc.xml $SCRIPT_DIR
-cp -u --parents .config/flake8 $SCRIPT_DIR
-cp -u --parents .config/redshift.conf $SCRIPT_DIR
-cp -R -u --parents .config/lxpanel/default/ $SCRIPT_DIR
-cp -R -u --parents .config/zim/ $SCRIPT_DIR
-cp -u --parents .config/fish/{functions/,}*.fish $SCRIPT_DIR
+
+DBG=${DBG:-}
+
+typeset -a dotfiles
+dotfiles=(
+	.*aliases
+	.shellrc
+	.bashrc
+	.screenrc
+	.vimrc
+	.zsh*
+	.*.zsh
+	.tmux*.conf
+	.xbindkeysrc*
+	.Xresources*
+	.xinitrc
+	.tidyrc
+	.editorconfig
+	.jshintrc
+	.eslintrc
+	.gitconfig
+	.gitignore.global
+	.ackrc
+	.npm_lazy.config.js
+	.npmrc
+	.mongorc.js
+	.env
+	.tmuxp
+	.noderc.js
+	.gtkrc-2.0*
+
+	.config/fish/{functions/,}*.fish
+	.config/openbox/rc.xml
+	.config/flake8
+	.config/redshift.conf
+	.config/lxpanel/default/
+	.config/zim/
+	.config/i3/
+)
+
+#echo $dotfiles
+#cp -u -R "$HOME/$dotfiles" $SCRIPT_DIR
+
+for dotfile in $dotfiles; do
+	[ -n "$DBG" ] && echo "Copying $dotfile"
+	cp -u --parents -R $dotfile $SCRIPT_DIR
+done;
+
+#cp -u --parents .config/openbox/rc.xml $SCRIPT_DIR
+#cp -u --parents .config/flake8 $SCRIPT_DIR
+#cp -u --parents .config/redshift.conf $SCRIPT_DIR
+#cp -R -u --parents .config/lxpanel/default/ $SCRIPT_DIR
+#cp -R -u --parents .config/zim/ $SCRIPT_DIR
+#cp -R -u --parents .config/i3/ $SCRIPT_DIR
+
+#cp -u --parents .config/fish/{functions/,}*.fish $SCRIPT_DIR
 fish -c 'abbr -s > .config/fish/fish-abbr.fish' &
 
 #cp -R ~/.vim/ .
